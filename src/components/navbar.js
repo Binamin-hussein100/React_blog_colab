3568328281a96e12ae465e9e87f47363a3b56f59
Binamin <82438528+Binamin-hussein100@users.noreply.github.com>
2023-08-23 16:44:55 +0300

    Bin/navbar (#6)
    * feat: install react-router-dom for easier navigation
    
    * feat: add and style navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+
+//NAVBAR
+    // appending the navbar
+    //  React router dom
+const Navbar = () =>{
+    return (
+        <div className="navbar-contaainer">
+            <div className="intro"></div>
+            <div className="navbar-list">
+                <h4>Home</h4>
+                <h4>Blogs</h4>
+                <h4>Certificates</h4>
+                <h4>Inbox</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
+
