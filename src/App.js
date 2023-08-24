9f5c2df63737dbf9474c16efd55e64270790738d
WanjikuN <70173660+WanjikuN@users.noreply.github.com>
2023-08-24 16:24:06 +0300

    feat: Add certificates (#9)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import HomePage from './components/HomePage';
+import Certificates from './components/Certificates';
 
 function App() {
   return (
     <div >
       <Navbar/>
       <HomePage />
+      <Certificates/>
 
     </div>
   );
