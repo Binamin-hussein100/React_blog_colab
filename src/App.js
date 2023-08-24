c89d09fd191a2db9842cf24c28a8d44fedfdfd13
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-08-24 17:16:45 +0300

    Inbox.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import HomePage from './components/HomePage';
+import Inbox from './components/Inbox';
 
 function App() {
   return (
     <div >
       <Navbar/>
       <HomePage />
+      <Inbox />
 
     </div>
   );
