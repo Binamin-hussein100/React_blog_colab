9e3ec9120c269f7b89dad22fa13be792e097c5aa
WanjikuN <cikunjoroge4@gmail.com>
2023-08-24 18:03:28 +0300

    Add styling to certificates

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,32 +1,24 @@
 import {useEffect, useState} from "react";
+import Certificate from "./certificate";
 function Certificates(){
     const [certificates, setCertificates] = useState([]);
     useEffect(()=>{
-        fetch("http://localhost:8080/certificates")
-        .then(res=> res.json())
-        .then(data=> setCertificates(data))
+         fetch("http://localhost:3030/certificates")
+         .then(resp => resp.json())
+         .then(data => setCertificates(data))   
     },[])
-        
-    return(
-        <div id="cert-container">
-            <div id="title-container">
-                <h1>Visit <span style={{color:"lightblue"}}>Gallery</span></h1>
-                <h2>Certificates</h2>
-
-            </div>
-            <div id="certificates">
-              {certificates.map((certificate) =>(
-                 <div key={certificate.id}>
-                     <h2>{certificate.title}</h2>
-                     <h4>{certificate.issuer}</h4>
-                     <h4>{certificate.date}</h4>
-                     <p>{certificate.description}</p>
-                 </div>
-              )
-                
-              )}   
-            </div>
+ return (
+    <div id="cert-container">
+        <div className="top">
+            <h2>Visit <span style={{color:"SkyBlue"}}>Gallery</span></h2>
+            <h3>Certificates</h3>
         </div>
-    )
+        <div className="bottom">
+         {certificates.map(certificate=>{
+             return <Certificate key={certificate.id} title={certificate.title} date={certificate.date} issuer={certificate.issuer} description={certificate.description} />
+         })}
+        </div>
+    </div>
+ )
 }
 export default Certificates;
